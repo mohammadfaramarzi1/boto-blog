b3a141ed4dc08cc17365b35222282798406f4a59
mohammadfaramarzi1 <faramarzi.dev@gamil.com>
2024-11-20 10:27:18 +0330

    add fonts to my project and change default theme configurations

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import { ThemeProvider } from "@mui/material";
+import theme from "./mui/theme";
+import App from "./App";
+
+import "./styles/index.css";
+import "./styles/fonts.css";
 
 const client = new ApolloClient({
   uri: "https://us-west-2.cdn.hygraph.com/content/cm3n7d0h508wz07ta0x8hz07y/master",
@@ -12,6 +17,8 @@ const client = new ApolloClient({
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <ApolloProvider client={client}>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </ApolloProvider>
 );
