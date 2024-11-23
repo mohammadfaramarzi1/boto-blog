0b6ba6758bebd09303e5e4f50e4fbe53f0b53078
mohammadfaramarzi1 <faramarzi.dev@gamil.com>
2024-11-23 07:49:06 +0330

    add SEND_COMMENT query

---
+++ src/graphql/mutations.js
@@ -0,0 +1,18 @@
+import { gql } from "@apollo/client";
+
+const SEND_COMMENT = gql`
+  mutation createComment($name: String!, $email: String!, text: String!, slug: String!) {
+    createComment(
+      data: {
+        name: $name
+        email: $email
+        text: $text
+        comments: { connect: { Post: { slug: $slug } } }
+      } 
+    ) {
+        id
+      }
+  }
+`;
+
+export {SEND_COMMENT}
